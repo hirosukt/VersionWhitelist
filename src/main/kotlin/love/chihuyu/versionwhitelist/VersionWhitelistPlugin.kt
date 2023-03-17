4594dddb90c6bf49e1afce682a64c6f06d1e0627
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2023-03-18 06:23:15 +0900

    main class

---
+++ src/main/kotlin/love/chihuyu/versionwhitelist/VersionWhitelistPlugin.kt
@@ -0,0 +1,14 @@
+package love.chihuyu.versionwhitelist
+
+import net.md_5.bungee.api.plugin.Plugin
+
+
+class VersionWhitelistPlugin: Plugin() {
+    companion object {
+        lateinit var VersionWhitelistPlugin: Plugin
+    }
+
+    init {
+        VersionWhitelistPlugin = this
+    }
+}
